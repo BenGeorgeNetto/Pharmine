e74cb559c2ffd9d6f688a75511214798f91ee61c
Ben George Netto <bengnetto245@gmail.com>
2022-11-06 00:00:49 +0530

    Added Color Scheme

--- app/src/main/java/com/example/pharmine/ui/theme/Theme.kt
+++ app/src/main/java/com/example/pharmine/ui/theme/Theme.kt
@@ -3,11 +3,7 @@ package com.example.pharmine.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
@@ -16,25 +12,23 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = PastelPink,
+    secondary = PastelBlue,
+    tertiary = LightGray,
+    background = Gray,
+    surface = PastelYellow,
+    inversePrimary = CloudyWhite,
+    onBackground = CloudyWhite
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = PastelPink,
+    secondary = PastelBlue,
+    tertiary = LightGray,
+    background = CloudyWhite,
+    surface = PastelYellow,
+    inversePrimary = Gray,
+    onBackground = Gray
 )
 
 @Composable
