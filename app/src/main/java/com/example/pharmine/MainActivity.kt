94e3dce86f98721c64e14bb1154471d767f6cb1b
Noel John Robert <noel060202@gmail.com>
2022-11-06 07:57:11 +0530

    added appointments page

--- app/src/main/java/com/example/pharmine/MainActivity.kt
+++ app/src/main/java/com/example/pharmine/MainActivity.kt
@@ -31,7 +31,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    PharmineApp()
+//                    PharmineApp()
+                    Appointments()
                 }
             }
         }
