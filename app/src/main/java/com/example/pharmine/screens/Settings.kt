f1b630ed9d84d1858ebd6b9d4600d49944030a7c
Samuel John <samjohnmalayam@gmail.com>
2022-11-06 13:39:43 +0530

    Settings

---
+++ app/src/main/java/com/example/pharmine/screens/Settings.kt
@@ -0,0 +1,126 @@
+package com.example.pharmine.screens
+
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pharmine.R
+import com.example.pharmine.models.user.SettingsViewModel
+import com.example.pharmine.ui.theme.PastelBlue
+import com.example.pharmine.ui.theme.poppinsFamily
+
+
+@Composable
+fun Settings(settingsViewModel: SettingsViewModel = viewModel()) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.pharmine_logo),
+                contentDescription = "logo",
+                modifier = Modifier
+                    .size(120.dp)
+                    .padding(8.dp),
+                colorFilter = ColorFilter.tint(PastelBlue)
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Pharmine",
+                fontFamily = poppinsFamily,
+                color = MaterialTheme.colorScheme.onBackground,
+                fontWeight = FontWeight.Bold,
+                fontSize = 32.sp,
+            )
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(
+            text = "Settings",
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onBackground,
+            fontSize = 38.sp
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 32.dp, vertical = 16.dp),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            Text(
+                text = "Emergency contact",
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onBackground,
+                fontSize = 22.sp,
+            )
+            Text(
+                text = "Payment Settings",
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onBackground,
+                fontSize = 27.sp,
+            )
+            Text(
+                text = "Payment Options",
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onBackground,
+                fontSize = 22.sp,
+            )
+            Text(
+                text = "Subscriptions",
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onBackground,
+                fontSize = 22.sp,
+            )
+
+            Text(
+                text = "Notification Settings",
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onBackground,
+                fontSize = 27.sp,
+            )
+            Text(
+                text = "Alarm",
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onBackground,
+                fontSize = 22.sp,
+            )
+            Text(
+                text = "Notifications",
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onBackground,
+                fontSize = 22.sp,
+            )
+
+            Text(
+                text = "Logout",
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onBackground,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold)
+        }
+    }
+}
